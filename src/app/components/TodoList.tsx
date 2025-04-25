dd712bc122db8e25f655d8be788cc048c87e66f5
sako <koshi051511@icloud.com>
2025-04-25 17:12:18 +0900

    💄 Todoリストの見た目を整えた

---
+++ src/app/components/TodoList.tsx
@@ -0,0 +1,27 @@
+export const TodoList = () => {
+  return (
+    <ul className="space-y-3">
+      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+        <span>散歩</span>
+        <div>
+          <button className="text-green-500 mr-3">edit</button>
+          <button className="text-red-500">delete</button>
+        </div>
+      </li>
+      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+        <span>勉強</span>
+        <div>
+          <button className="text-green-500 mr-3">edit</button>
+          <button className="text-red-500">delete</button>
+        </div>
+      </li>
+      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+        <span>買い物</span>
+        <div>
+          <button className="text-green-500 mr-3">edit</button>
+          <button className="text-red-500">delete</button>
+        </div>
+      </li>
+    </ul>
+  );
+};
