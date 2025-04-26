2847ea6ec50c4245d1980a46522d8e4272a7cd7a
sako <koshi051511@icloud.com>
2025-04-26 15:51:07 +0900

    ✨ Todoリストに追加、編集、削除機能を導入しました。

--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -1,8 +1,28 @@
+"use client";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { addTodo } from "../api";
+
 export const AddTask = () => {
+  const [todoTitle, setTodoTitle] = useState<string>("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    await addTodo({ id: Date.now(), title: todoTitle });
+  };
+
   return (
-    <form className="mb-4 space-y-3">
-      <input type="text" className="w-full border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400"/>
-      <button className="w-full px-4 py-2 text-white bg-blue-500 rounded transform hover:bg-blue-400 hover:scale-95 duration-200">Add Task</button>
+    <form className="mb-4 space-y-3" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="w-full border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400"
+        value={todoTitle}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setTodoTitle(e.target.value)
+        }
+      />
+      <button className="w-full px-4 py-2 text-white bg-blue-500 rounded transform hover:bg-blue-400 hover:scale-95 duration-200">
+        Add Task
+      </button>
     </form>
   );
-}
+};
--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,27 +1,16 @@
-export const TodoList = () => {
+import { Todo } from "../types";
+import { TodoAction } from "./TodoAction";
+
+export interface TodoListProps {
+  todos: Todo[];
+}
+
+export const TodoList = ({ todos }: TodoListProps) => {
   return (
     <ul className="space-y-3">
-      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-        <span>散歩</span>
-        <div>
-          <button className="text-green-500 mr-3">edit</button>
-          <button className="text-red-500">delete</button>
-        </div>
-      </li>
-      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-        <span>勉強</span>
-        <div>
-          <button className="text-green-500 mr-3">edit</button>
-          <button className="text-red-500">delete</button>
-        </div>
-      </li>
-      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-        <span>買い物</span>
-        <div>
-          <button className="text-green-500 mr-3">edit</button>
-          <button className="text-red-500">delete</button>
-        </div>
-      </li>
+      {todos.map((todo) => (
+        <TodoAction key={todo.id} todo={todo} />
+      ))}
     </ul>
   );
 };
